8c5c33c8718fd7f5d236f5e5168b6fa8c468ca68
Teddy Zamborsky <tzamborsky@uchicago.edu>
2016-08-31 12:50:08 -0400

    made interactive

--- eng_entropy.py
+++ eng_entropy.py
@@ -1,5 +1,8 @@
 import math
 
+q = raw_input("Enter a string to do calculations on: ")
+command = raw_input("Enter 'entropy' to calculate string entropy, 'frequency' to calculate letter frequency, and 'english entropy estimate to get an estimate for english language entropy': ")
+
 def ent_eng():
     prob_list = [.08167,.01492, .02782, .04253, .12702, .02228, .02015, .06094, .06966, .000153, .00772, .04025, .02406, .06749, .07507, .01929, .00095, .05987, .06327, .09056, .02758, .00978, .02361, .00150, .01974, .00074]
     res = 0
@@ -8,7 +11,7 @@ def ent_eng():
         hold_list.append(i * math.log(i,2))
     res = -sum(hold_list)
     return res
-print ent_eng()
+
 
 def reader(x_string):
     diction = {}
@@ -34,6 +37,15 @@ def entropy_est(x_string):
         hold_list.append(i * math.log(i,2))
     res = -sum(hold_list)
     return res
-print reader("teddy")
-print frequency("teddy")
-print entropy_est("teddy")
+def interpreter(inputs):
+    inputs = inputs.lower()
+    if(inputs == "entropy"):
+        print "The entropy estimate of %s is %f" %(q, entropy_est(q))
+    elif(inputs == "english entropy estimate"):
+        print "The entropy estimate of English, only taking letter frequency into account, is %f" %ent_eng()
+    elif(inputs == "frequency"):
+        print "The frequency of each letter in your input string is as follows: "
+        print frequency(q)
+    else:
+        print "Error, enter a valid command"
+print interpreter(command)
